be97b8d1e31cc9c8dfdf23489179721d74cfc8ca
Korre19 <korneel.jambon@telenet.be>
2021-03-06 16:42:02 +0100

    Werkende code

--- Distance-sensor.py
+++ Distance-sensor.py
@@ -1,51 +1,14 @@
-import RPi.GPIO as gpio
-import time
+from machine import UART
 
-def distance(measure='cm'):
-    try:
-        gpio.setmode(gpio.BOARD)
-        gpio.setup(12, gpio.OUT)
-        gpio.setup(16, gpio.IN)
+uart = UART(1)
+uart.init(baudrate=9600, bits=8, parity=None, stop=1, timeout_chars=190)
 
-        gpio.output(12, False)
-        while gpio.input(16) == 0:
-            nosig = time.time()
-
-        while gpio.input(16) == 1:
-            sig = time.time()
-
-        tl = sig - nosig
-
-        if measure == 'cm':
-            distance = tl / 0.000058
-        elif measure == 'in':
-            distance = tl / 0.000148
-        else:
-            print('improper choice of measurement: in or cm')
-            distance = None
-
-        gpio.cleanup()
-        return distance
-    except:
-        distance = 100
-        gpio.cleanup()
-        return distance
-
-
-if __name__ == "__main__":
-    print(distance('cm'))
-
-main
-
-import time
-import board
-import busio
-import adafruit_vl53l0x
-
-# Initialize I2C bus and sensor.
-i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-while True:
-print("Range: {0}mm".format(vl53.range))
-time.sleep(1.0) 
-Distance-sensor
+while True: #Oneindige loop
+    header_bytes = uart.read(1)
+    data_header = int(uart.read(1)[0])
+    data_high = int(uart.read(2)[0])
+    data_low = int(uart.read(3)[0])
+    distance = data_high*256 + data_low
+    #Is de data/Getal kleiner dan 1000 dan print hij die anders print hij niets
+    if int(distance/10) < 1000:
+        print("afstand",int(distance/10), "cm") #print "afstand" + getal + "cm"
